41d0a4f73e91c7bbc53cfe83bd90c99bc233e738
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-12 18:48:31 +0700

    Add files via upload

---
+++ frontend/diy/App.vue
@@ -0,0 +1,28 @@
+<script>
+//create by: 邓志锋 <280160522@qq.com> <https://www.diygw.com> DIYGW可视化设计一键生成源码
+export default {
+    globalData: {
+        userInfo: null,
+        tabBar:["/pages/index","/pages/compoment","/pages/page/page","/pages/about"],
+        homePage:"/pages/index",
+        pages:["/pages/index","/pages/page/page","/pages/app/index","/pages/app/shop","/pages/zsff/zsff","/pages/zsff/zsfflb","/pages/zsff/zsffsq","/pages/zsff/zsffgl","/pages/layout","/pages/button","/pages/avatar","/pages/tag","/pages/title","/pages/grid","/pages/list","/pages/image","/pages/swiper","/pages/timeline","/pages/steps","/pages/modal","/pages/tab","/pages/about","/pages/buttons","/pages/rate","/pages/progress","/pages/notice","/pages/absolute","/pages/collapse","/pages/audio","/pages/cubes","/pages/compoment","/pages/swipers","/pages/forms","/pages/floatbar"]
+    },
+    onLaunch: function () {
+    },
+    onShow() {
+    },
+    onHide() {
+    }
+}
+</script>
+<style lang="scss">
+	/*!
+     * Site:http://www.diygw.com
+     * Author:邓志锋 280160522
+     */
+	@import 'common/diygw-ui/iconfont.scss';
+	@import 'common/diygw-ui/index.scss';
+
+	@import './uni_modules/diy-uview-ui/index.scss';
+	@import 'common/diygw-ui/uview.scss';
+</style>
