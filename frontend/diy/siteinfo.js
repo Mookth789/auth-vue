58e190a1e03226232d6f42e5b179129b6fc644b1
mook Th <mook.th0909@gmail.com>
2023-08-12 13:03:02 +0000

    Delete 250 files

--- frontend/diy/siteinfo.js
+++
@@ -1,9 +0,0 @@
-//Update: 邓志锋 <280160522@qq.com> <http://www.diygw.com>
-export default {
-    basePath: 'http://www.diygw.com',
-    fileBasePath: 'http://lib.diygw.com',
-    title: 'DIYGWUI',
-    debug: true,
-    dashboardid:'7',
-    mpid:''
-}
